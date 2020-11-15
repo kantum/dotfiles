759725ee51219d23de099c78f0eadfa24146eee1
kantum <43590963+kantum@users.noreply.github.com>
2020-11-15 15:31:03 +0100

    Update manjaro_init.sh

--- manjaro/manjaro_init.sh
+++ manjaro/manjaro_init.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Dotfiles path
-DOTFILES=$HOME/dotfiles
 # Applications repository path
 REPOSITORY=$HOME/depots
+
+# Make depots folder
+mkdir -p $REPOSITORY
+
+# Dotfiles path
+DOTFILES=$REPOSITORY/dotfiles
+
 # Pacman command
 PACMAN="pacman --noconfirm --needed -S"
 
+echo "\e[32m\n### PASSWORD ###\n\e[0m"
 sudo -v
 # Update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "\e[32m\n### GIT CLONE ###\n\e[0m"
 # Download dotfiles
 git clone https://github.com/kantum/dotfiles $DOTFILES 2> /dev/null
 
@@ -21,36 +28,33 @@ sudo systemctl start fstrim.timer
 # Disable terminal bip
 xset -b
 
+echo "\e[32m\n### UPDATE EXISTING ###\n\e[0m"
 # Update installed packages
 sudo pacman -Syu --noconfirm --needed
 
-# Install basic development tools
-sudo $PACMAN base-devel
-
-# Make depots folder
-mkdir -p $REPOSITORY
-
-# Remove old vim
-sudo pacman --noconfirm -Rs vim
-
+echo "\e[32m\n### INSTALL OFFICIAL ###\n\e[0m"
 # Install official supported packages
 echo "Install official packages"
 sudo $PACMAN - < $DOTFILES/applications/official.list
 
+echo "\e[32m\n### ENABLE FIREWALL ###\n\e[0m"
 # Enable firewall
 sudo systemctl enable ufw
 sudo ufw enable
 
+echo "\e[32m\n### INSTALL AUR ###\n\e[0m"
 # Install AUR Packages
 echo "Install AUR packages"
 yay -S --needed --noconfirm - < $DOTFILES/applications/aur.list
 
+echo "\e[32m\n### DEFAULT SHELL ###\n\e[0m"
 # Use zsh as default shell
 sudo chsh -s /bin/zsh $USER
 
 # Add user to the uucp group
 sudo usermod -a -G uucp $USER
 
+echo "\e[32m\n### STOW DOTFILES ###\n\e[0m"
 rm  -rf \
 $HOME/.i3/config \
 $HOME/.zshrc \
@@ -61,7 +65,6 @@ $HOME/.config/vlc \
 $HOME/.config/redshift.conf \
 $HOME/.config/zathura \
 $HOME/.config/sway \
-
 
 cd $DOTFILES
 stow -t $HOME \
@@ -75,6 +78,7 @@ stow -t $HOME \
 	zathura \
 	sway
 
+echo "\e[32m\n### INSTALL VIM ###\n\e[0m"
 # Install vim-plug and plugins
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
