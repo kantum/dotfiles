e57f6de56141128d8c6897fb541948a23ad9f830
kantum <quentin@durot.co>
2023-06-29 20:28:30 +0200

    fix: remove duplicated config

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -69,24 +69,6 @@ return {
 		end,
 	},
 	{ 'nvim-tree/nvim-tree.lua' },
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"debugloop/telescope-undo.nvim",
-		},
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					undo = {
-						-- telescope-undo.nvim config, see below
-					},
-				},
-			})
-			require("telescope").load_extension("undo")
-			-- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
-		end,
-	},
 	{'akinsho/toggleterm.nvim', version = "*", config = true},
 	{ 'lewis6991/gitsigns.nvim' },
 	{ 'neovim/nvim-lspconfig' },
