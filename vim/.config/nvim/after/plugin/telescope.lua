ee0f277b783082edba9722bc4362792a6e80e01a
Kantum <42kantum@gmail.com>
2023-10-10 16:40:15 +0200

    fix: still not showing hidden files

--- vim/.config/nvim/after/plugin/telescope.lua
+++ vim/.config/nvim/after/plugin/telescope.lua
@@ -18,8 +18,6 @@ require("telescope").setup({
 		},
 		pickers = {
 			find_files = {
-				hidden = true, -- will still show the inside of `.git/` as it's not `.gitignore`d.
-				-- find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
 				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-u" },
 			},
 		},
@@ -68,4 +66,3 @@ function vim.getVisualSelection()
 		return ""
 	end
 end
-
