ef4a6c7ffde294a9da2ee966a69532d32f27f4e7
Kantum <42kantum@gmail.com>
2023-11-29 00:29:58 +0100

    feat: add convco globally

--- nix/.config/home-manager/home.nix
+++ nix/.config/home-manager/home.nix
@@ -23,6 +23,7 @@
     direnv
     ffmpeg
     mpv
+    convco
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
