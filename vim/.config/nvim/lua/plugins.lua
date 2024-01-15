524519b5d2700c95f416ecdaaf36a1f751a1e6c9
kantum <42kantum@gmail.com>
2024-01-15 21:16:52 +0100

    fix: move copilot conf

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -25,51 +25,6 @@ return {
 	{
 		"zbirenbaum/copilot.lua",
 		build = "Copilot",
-		config = function()
-			require("copilot").setup({
-				panel = {
-					enabled = true,
-					auto_refresh = false,
-					keymap = {
-						jump_prev = "[[",
-						jump_next = "]]",
-						accept = "<CR>",
-						refresh = "gr",
-						open = "<M-CR>",
-					},
-					layout = {
-						position = "bottom", -- | top | left | right
-						ratio = 0.4,
-					},
-				},
-				suggestion = {
-					enabled = true,
-					auto_trigger = true,
-					debounce = 75,
-					keymap = {
-						accept = "<tab>",
-						accept_word = false,
-						accept_line = false,
-						next = "<M-]>",
-						prev = "<M-[>",
-						dismiss = "<C-]>",
-					},
-				},
-				filetypes = {
-					yaml = true,
-					-- markdown = false,
-					-- help = false,
-					-- gitcommit = false,
-					-- gitrebase = false,
-					-- hgcommit = false,
-					-- svn = false,
-					-- cvs = false,
-					-- ["."] = false,
-				},
-				copilot_node_command = "node", -- Node.js version must be > 16.x
-				server_opts_overrides = {},
-			})
-		end,
 	},
 	{
 		"jonahgoldwastaken/copilot-status.nvim",
@@ -222,4 +177,26 @@ return {
 		},
 	},
 	{ "digitaltoad/vim-pug" },
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^3", -- Recommended
+		ft = { "rust" },
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup()
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
 }
