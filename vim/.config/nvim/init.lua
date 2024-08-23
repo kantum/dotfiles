62863060d68961fe1a1ee975d17023cac4f12f65
kantum <42kantum@gmail.com>
2024-08-23 02:49:01 +0200

    feat: add auto-reload on file external change

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -16,6 +16,14 @@ vim.g.mapleader = " "
 -- Avoid creating swap files in case lazy fails to load
 vim.cmd(":set noswapfile")
 
+-- auto-reload files when modified externally
+-- https://unix.stackexchange.com/a/383044
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
+})
+
 -- Add lazy to runtimepath
 vim.opt.rtp:prepend(lazypath)
 
