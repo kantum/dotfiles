6b60a697dd3b5ae5723af1d60e393b60bf3c3839
kantum <42kantum@gmail.com>
2025-03-16 22:28:51 +0100

    fix: markdown problem

--- vim/.config/nvim/after/plugin/treesitter.lua
+++ vim/.config/nvim/after/plugin/treesitter.lua
@@ -55,7 +55,7 @@ require("nvim-treesitter.configs").setup({
 		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
 		-- the name of the parser)
 		-- list of language that will be disabled
-		disable = { "c", "rust", "latex", "just" },
+		disable = { "c", "rust", "latex", "just", "markdown" },
 		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 		-- disable = function(lang, buf)
 		-- 	local max_filesize = 100 * 1024 -- 100 KB
--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -120,7 +120,6 @@ return {
 		ft = { "go", "gomod" },
 		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
-	{ "windwp/nvim-ts-autotag" },
 	-- LSP Support
 	{
 		"neovim/nvim-lspconfig",
