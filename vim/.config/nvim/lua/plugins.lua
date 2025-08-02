3dec2d28ff29c04a9c430b9465cc5e6fde108a39
kantum <42kantum@gmail.com>
2025-08-02 23:22:13 +0200

    fix: avante config

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -282,14 +282,14 @@ return {
 					},
 				},
 			},
-			-- {
-			-- 	-- Make sure to set this up properly if you have lazy=true
-			-- 	"MeanderingProgrammer/render-markdown.nvim",
-			-- 	opts = {
-			-- 		file_types = { "markdown", "Avante" },
-			-- 	},
-			-- 	ft = { "markdown", "Avante" },
-			-- },
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
 		},
 	},
 
