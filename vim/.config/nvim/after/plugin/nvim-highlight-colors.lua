be6432537cbd9e97f95068deb6e8a955d8a81529
kantum <42kantum@gmail.com>
2025-10-20 12:00:18 +0200

    fix: remove custom colors that conflict with themes

--- vim/.config/nvim/after/plugin/nvim-highlight-colors.lua
+++ vim/.config/nvim/after/plugin/nvim-highlight-colors.lua
@@ -47,8 +47,8 @@ require("nvim-highlight-colors").setup({
 	---Label must be properly escaped with '%' to adhere to `string.gmatch`
 	--- :help string.gmatch
 	custom_colors = {
-		{ label = "%-%-theme%-primary%-color", color = "#0f1219" },
-		{ label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
+		-- { label = "%-%-theme%-primary%-color", color = "#0f1219" },
+		-- { label = "%-%-theme%-secondary%-color", color = "#5a5d64" },
 	},
 
 	-- Exclude filetypes or buftypes from highlighting e.g. 'exclude_buftypes = {'text'}'
